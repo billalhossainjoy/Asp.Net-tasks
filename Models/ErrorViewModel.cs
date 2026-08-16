4bed42dcbcbc9d4486ee2e7332b55d463a17b78c
Md Billal Hossain Joy <96815319+billalhossainjoy@users.noreply.github.com>
2026-08-16 07:24:08 +0000

    update

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Asp.Net_task3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
-using System.Numerics;
-using System.Globalization;
-
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/app", () =>
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    return "billal"
-});
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
 
 app.Run();
