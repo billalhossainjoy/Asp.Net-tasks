28f735849cdebb641c1efb0d8c8b8a522eb4ed0b
billalhossainjoy <contact@billalhossain.dev>
2026-08-17 16:03:51 +0000

    feat: add pgadmin and add db context service

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
