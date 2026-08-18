5a9db5aebe2c4da8b0703d346ccae41a9b0fd428
billalhossainjoy <contact@billalhossain.dev>
2026-08-18 21:36:32 +0000

    feat: user list and add, update, delete toolbar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Asp.Net_tasks.Models;
 using Microsoft.EntityFrameworkCore;
 using Asp.Net_tasks.ViewModel.Home;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace Asp.Net_tasks.Controllers;
 
@@ -27,6 +29,112 @@ public class HomeController : Controller
         }).ToListAsync();
         return View(users);
     }
+
+[HttpPost]
+[ValidateAntiForgeryToken]
+    public async Task<IActionResult> Block(List<Guid> selectedIds)
+    {
+   var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+   if (!Guid.TryParse(userId, out var currentUserId))
+        {
+            return Unauthorized();
+        }
+
+        if (selectedIds.Count == 0)
+        {
+        TempData["ErrorMessage"] = "Please select at least one user.";
+            return RedirectToAction(nameof(Index));
+        }
+
+        var users = await _db.Users.Where(x => selectedIds.Contains(x.Id)).ToListAsync();
+
+        foreach (var user in users)
+            {
+                if (user.Id == currentUserId)
+                {
+                    continue;
+                }
+                user.Status = UserStatus.Blocked;
+            }
+
+
+    await _db.SaveChangesAsync();
+
+
+    TempData["SuccessMessage"] =
+        $"{users.Count} user(s) blocked successfully.";
+
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> UnBlock(List<Guid> selectedIds)
+    {
+    if (selectedIds.Count == 0)
+    {
+        TempData["ErrorMessage"] =
+            "Please select at least one user.";
+
+        return RedirectToAction(nameof(Index));
+    }
+
+        var users = _db.Users.Where(x => selectedIds.Contains(x.Id)).ToList();
+
+
+        foreach(var user in users)
+        {
+            user.Status = UserStatus.Active;
+        }
+
+        await _db.SaveChangesAsync();
+
+        TempData["SuccessMessage"] =
+        $"{users.Count} user(s) unblocked successfully.";
+
+        return RedirectToAction(nameof(Index));
+    }
+
+[HttpPost]
+[ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete (List<Guid> selectedIds)
+    {
+    if (selectedIds.Count == 0)
+    {
+        TempData["ErrorMessage"] =
+            "Please select at least one user.";
+
+        return RedirectToAction(nameof(Index));
+    }
+
+        var users = await _db.Users
+        .Where(x => selectedIds.Contains(x.Id))
+        .ToListAsync();
+
+
+    if (users.Count == 0)
+    {
+        TempData["ErrorMessage"] =
+            "No users were found.";
+
+        return RedirectToAction(nameof(Index));
+    }
+
+
+    _db.Users.RemoveRange(users);
+
+    await _db.SaveChangesAsync();
+
+        TempData["SuccessMessage"] =
+        $"{users.Count} user(s) deleted successfully.";
+
+
+        return RedirectToAction(nameof(Index));
+    }
+
+
 
 
 
